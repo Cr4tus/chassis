336dea8cb5d932bc819dc79a609a298422aa53b1
cr4tus <danioansabau@icloud.com>
2025-12-19 18:32:09 +0200

    [fix] Removed index.ts from subdirectories

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,7 @@
-export * from "./types";
-export * from "./utils";
+export * from "./types/enums";
+export * from "./types/db/core";
+export * from "./types/db/users";
+export * from "./types/db/requirements";
+
+export * from "./utils/schemas";
+export * from "./utils/functions";
--- src/types/db/index.ts
+++
@@ -1,3 +0,0 @@
-export * from "./core";
-export * from "./users";
-export * from "./requirements";
