efb9f48eec054f8f820b1ac7964ee05fc8ea0345
cr4tus <danioansabau@icloud.com>
2026-01-25 19:07:49 +0200

    [small-fix] Export new types from index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,12 @@
 export * from "./types/auth.js";
 export * from "./types/enums.js";
+export * from "./types/vehicle.js";
+export * from "./types/document.js";
 export * from "./types/db/core.js";
 export * from "./types/db/users.js";
 export * from "./types/db/messages.js";
 export * from "./types/db/requirements.js";
+export * from "./types/db/opt-in/moving.js";
 
 export * from "./utils/schemas.js";
 export * from "./utils/functions.js";
